99745ced9439066a9b462fdbcb0f39d10939ae50
abhijeet <abhiojeetpandeydhn@gmail.com>
2023-04-23 18:37:14 +0530

    first commit

---
+++ app/src/main/java/com/example/vitbhopal/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vitbhopal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.vitbhopal.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+
+
+    }
+}
