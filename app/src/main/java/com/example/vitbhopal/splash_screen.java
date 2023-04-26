0807c8b131aa85e93b203b094b8ea4a9bbbe7e2c
abhijeet <abhiojeetpandeydhn@gmail.com>
2023-04-26 15:19:15 +0530

    added main activiety page

--- app/src/main/java/com/example/vitbhopal/splash_screen.java
+++ app/src/main/java/com/example/vitbhopal/splash_screen.java
@@ -2,7 +2,9 @@ package com.example.vitbhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(iHome);
+            }
+        },3000);
+
     }
 }
