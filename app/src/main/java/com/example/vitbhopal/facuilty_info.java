c879655fc49e43361c0eab2f1d56ac2eb19a91c4
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-04 18:13:10 +0530

    updated all data

--- app/src/main/java/com/example/vitbhopal/facuilty_info.java
+++ app/src/main/java/com/example/vitbhopal/facuilty_info.java
@@ -1,42 +1,52 @@
 package com.example.vitbhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.vitbhopal.adapters.facultyInfoAdapters;
+import com.example.vitbhopal.models.facultyInfoModelClass;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class facuilty_info extends AppCompatActivity {
-ListView listView;
-ArrayList<String> arrNames = new ArrayList<>();
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    List<facultyInfoModelClass> userList;
+    facultyInfoAdapters facultyInfoAdapters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facuilty_info);
 
-        listView = findViewById(R.id.listView);
-
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-        arrNames.add("Ram");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, arrNames);
-        listView.setAdapter(adapter);
-
-
+        initData();
+        initRecyclerView();
     }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView1);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        facultyInfoAdapters = new facultyInfoAdapters(userList);
+        recyclerView.setAdapter(facultyInfoAdapters);
+        facultyInfoAdapters.notifyDataSetChanged();
+    }
+
+    private void initData() {
+        userList = new ArrayList<>();
+
+        userList.add(new facultyInfoModelClass("Name 1"));
+        userList.add(new facultyInfoModelClass("Name 2"));
+        userList.add(new facultyInfoModelClass("Name 3"));
+    }
+
+
 }
---
+++ app/src/main/java/com/example/vitbhopal/models/facultyInfoModelClass.java
@@ -0,0 +1,14 @@
+package com.example.vitbhopal.models;
+
+public class facultyInfoModelClass {
+
+    private String name;
+
+    public facultyInfoModelClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
