69fe6a5cb4c934ba57d3b962fb8f8539dee5b116
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-27 13:45:44 +0530

    added fragment in main activety

--- app/src/main/java/com/example/vitbhopal/bottom_nav.java
+++ app/src/main/java/com/example/vitbhopal/bottom_nav.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.vitbhopal.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class bottom_nav extends AppCompatActivity {
     BottomNavigationView bnView;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,11 @@ public class bottom_nav extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_nav);
 
         bnView = findViewById(R.id.bnView);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
     }
 }
